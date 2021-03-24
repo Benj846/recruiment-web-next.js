68494b0bbca955fa1204ec0aa651d7034b925d60
nineGenuinest <nine@genuinest.com>
2021-03-24 16:18:35 +0900

    Add bootstrap with customizing
    and install bootstrap-icons, sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
+import Layout from '../components/Layout';
+import '../styles/globals.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
