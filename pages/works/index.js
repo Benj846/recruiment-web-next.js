d60b238500de06885f146a601de26242f1be238b
Ben <rqdjbr@icloud.com>
2022-05-09 18:58:26 +0900

    disable works temp

--- pages/works/index.js
+++ pages/works/index.js
@@ -1,6 +1,6 @@
-import Meta from '../../components/Meta';
-import { gql } from '@apollo/client';
-import client from '../../apollo-client';
+import Meta from "../../components/Meta";
+import { gql } from "@apollo/client";
+import client from "../../apollo-client";
 
 const GET_USER = gql`
   query {
@@ -23,31 +23,27 @@ const Works = ({ user }) => {
   // const [addTodo, { data }] = useMutation(ADD_TODO);
 
   return (
-    <div className="h-80">
-      <Meta
-        title="Display Users"
-        keywords="getallusers"
-        description="display all the users"
-      />
-      <div className="min-w-min min-h-60">
-        {user.map((a) => (
+    <div className='h-80'>
+      <Meta title='Display Users' keywords='getallusers' description='display all the users' />
+      <div className='min-w-min min-h-60'>
+        {/* {user.map((a) => (
           <div key={a.Id}>{a.Name}</div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
 };
 
-export async function getStaticProps() {
-  const { data } = await client.query({
-    query: GET_USER,
-  });
+// export async function getStaticProps() {
+//   const { data } = await client.query({
+//     query: GET_USER,
+//   });
 
-  return {
-    props: {
-      user: data.getUserInfo,
-    },
-  };
-}
+//   return {
+//     props: {
+//       user: data.getUserInfo,
+//     },
+//   };
+// }
 
 export default Works;
